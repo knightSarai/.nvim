be71bdf866095eef28433f2b0ece0e03b9d513f3
knightSarai <knight.sarai.dev@gmail.com>
2022-11-20 16:51:13 +0300

    Telescop keymaps to look in cwd

--- lua/knight/telescope.lua
+++ lua/knight/telescope.lua
@@ -47,7 +47,6 @@ telescope.setup {
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
 
       n = {
