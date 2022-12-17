4b22271ca2e7e5c29ee570f24885be0977685546
knightSarai <knight.sarai.dev@gmail.com>
2022-12-18 02:16:57 +0300

    update lsp

--- lua/knight/copilot.lua
+++ lua/knight/copilot.lua
@@ -1,3 +1,4 @@
-vim.cmd[[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
+vim.cmd[[imap <silent><script><expr> <C-A> copilot#Accept("\<CR>")]]
 vim.g.copilot_no_tab_map = true
+
 
--- lua/knight/plugins.lua
+++ lua/knight/plugins.lua
@@ -16,12 +16,12 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Reloads neovim whenever save plugins.lua
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+--[[ vim.cmd [[ ]]
+--[[   augroup packer_user_config ]]
+--[[     autocmd! ]]
+--[[     autocmd BufWritePost plugins.lua source <afile> | PackerSync ]]
+--[[   augroup end ]]
+
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -38,6 +38,7 @@ packer.init {
    },
 }
 
+
 -- Plugins
 return packer.startup(function(use)
 
@@ -49,21 +50,35 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim"
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
-  use "neovim/nvim-lspconfig"
-  use "williamboman/nvim-lsp-installer"
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
+  }
+}
+
 
   -- Telescope
   use 'nvim-telescope/telescope.nvim'
